b2c0d387281d963f0fa8ad6b864ae6978a388dba
Maxime Penn <59399621+Youtmax654@users.noreply.github.com>
2023-11-22 18:38:56 +0100

    s9

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-from myImage import *
-from filters import *
+from cli import *
 
-#try:
-#   imgPath = input('Enter a path image: ')
-#except FileNotFoundError:
-#   print("No such file or directory.")
-
-image = MyImage("img/nishikata.jpg")
-image.pillowImg = GreyImage(image.pillowImg)
-image.pillowImg = BlurImage(image.pillowImg, 20)
-image.pillowImg = DilateImage(image.cv2Img)
-image.pillowImg = TextOnImage(image.pillowImg, "AAAAA", 256, (256, 256))
-image.pillowImg = RotateImage(image.pillowImg, 20)
-image.pillowImg = ResizeImage(image.pillowImg, (64, 64))
-image.SaveImage("azerty.jpg")
