450e7af9711bcd9bfb33b0c46ddb8f80816d336c
BaptisteCMY <baptiste.comoy2501@gmail.com>
2023-11-23 09:07:36 +0100

    (Bonus) Story 15: GIF export support

--- file.py
+++ file.py
@@ -2,6 +2,8 @@ import os
 import cv2 
 from PIL import Image
 
+from filters import *
+
 def OpenImage(path):
     """
     Create a pillow and cv2 image instance
@@ -37,3 +39,32 @@ def SaveImage(image, outPath):
         print("The output file extension is not supported.")
     except OSError:
         print("Something went wrong during the exportation.")
+
+
+def create_gif(image_paths, gif_path, duration=100, loop=0, resize_dimension=(500, 500)):
+    """
+    Create a GIF from a list of image file paths.
+
+    Parameters:
+    - image_paths: List of file paths to images.
+    - gif_path: Path to save the resulting GIF file.
+    - duration: Duration (in milliseconds) to display each frame.
+    - loop: Number of loops (0 for infinite loop).
+    - resize_dimension: New image dimension as a tuple (width, height) for resizing.
+
+    Returns:
+    - None
+    """
+    image_list = [Image.open(image_path) for image_path in image_paths]
+    for i in range(len(image_list)):
+        resized_image = ResizeImage(image_list[i], resize_dimension)
+        if resized_image is not None:
+            image_list[i] = resized_image
+    image_list[0].save(
+        gif_path,
+        save_all=True,
+        append_images=image_list[1:],
+        duration=duration,
+        loop=loop
+    )
+
--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from file import *
 from filters import *
 
-try:
-   imgPath = input('Enter a path image: ')
-except FileNotFoundError:
-   print("No such file or directory.")
 
-path = input('Enter a path image: ')
-image = OpenImage(path)
-image = GreyImage(image)
-image = BlurImage(image, 5)
-image = DilateImage(image)
-image = TextOnImage(image, "FOR GOD SAKE", 12, (40, 40), "green")
-image = RotateImage(image, 20)
-image = ResizeImage(image, (200, 100))
-image = WatercolourImage(image)
-SaveImage(image, "result.jpg")
+# path = input('Enter a path image: ')
+# image = OpenImage(path)
+# # image = GreyImage(image)
+# # image = BlurImage(image, 5)
+# # image = DilateImage(image)
+# # image = TextOnImage(image, "FOR GOD SAKE", 12, (40, 40), "green")
+# # image = RotateImage(image, 20)
+# # image = ResizeImage(image, (200, 100))
+# image = WatercolourImage(image)
+# SaveImage(image, "img/result.jpg")
+
+images_list = ['img/paysage1.jpg','img/paysage2.jpg','img/paysage3.jpg','img/paysage4.jpg','img/paysage5.jpg','img/paysage6.jpg']
+create_gif(images_list, 'img/paysage.gif', duration=500, loop=0)
